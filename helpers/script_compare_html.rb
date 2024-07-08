bf431bf3f2bb8009a15e1b208c9a281c6eeda9dc
Jose Lara <jvlara@uc.cl>
2024-07-08 09:46:54 -0400

    fix: comparing bugs

--- helpers/script_compare_html.rb
+++ helpers/script_compare_html.rb
@@ -1,8 +1,8 @@
 require_relative '../lib/query_counter'
 require 'pry-byebug'
 
-data_1 = File.read('helpers/example_data.yaml')
-data_2 = File.read('helpers/example_data_2.yaml')
+data_1 = File.read('helpers/automatic_lazily.yaml')
+data_2 = File.read('helpers/original.yaml')
 data_1 = YAML.safe_load(data_1, permitted_classes: [Proc, Symbol])
 data_2 = YAML.safe_load(data_2, permitted_classes: [Proc, Symbol])
 QueryCounter.compare do |comparing|
